9b12e790ea92bcba78dd707169833f05bb400cc0
Unknown <up201605330@g.uporto.pt>
2018-10-13 23:58:56 +0100

    Habemus Cylinder

--- LAIGDemoParser20182019v2/MyCylinder.js
+++ LAIGDemoParser20182019v2/MyCylinder.js
@@ -1,59 +1,70 @@
-class MyCylinder extends CGFobject{
+/**
+ * MyCylinder
+ * @constructor
+ */
+class MyCylinder extends CGFobject {
+  	constructor(scene, base, top, height, slices, stacks) {
+    	super(scene);
 
-	constructor(scene, base, top, height, slices, stacks){
+    	this.base = base;
+    	this.top = top;
+    	this.height = height;
+    	this.slices = slices;
+    	this.stacks = stacks;
 
-		super(scene);
+    	this.initBuffers();
+  	};
 
-		this.base = base;
-		this.top = top;
-		this.height = height;
-		this.slices = slices;
-		this.stacks = stacks;
+  	initBuffers() {
 
-		this.initBuffers();
-	}
+  		this.vertices = [];
+  		this.normals = [];
+  		this.indices = [];
+  		this.texCoords = [];
 
-	initBuffers(){
+  		this.getVertices();
 
-		this.vertices = [];
-		this.indices = [];
-		this.normals = [];
-		this.textCoords = [];
+  		this.primitiveType = this.scene.gl.TRIANGLES;
 
-		var ang = 2*Math.PI/this.slices;
-		var diffBaseTop = (this.base-this.top)/this.stacks;
-		var theta = Math.atan((this.base-this.top)/this.height);
+  		this.initGLBuffers();
+  	};
 
-		for(let j = 0; j <= this.stacks; j++){
-			for(let i = 0; i < this.slice; i++){
+  	getVertices() {
 
-				let ang1 = Math.cos(i*ang);
-				let ang2 = Math.sin(i*ang);
+    	var angle = (2 * Math.PI) / this.slices;
+    	var stackSize = this.height / this.stacks;
+    	var topBase = this.top-this.base;
+    	var diff = topBase/this.stacks; //Difference from Base to Top for Each Stack
+    	var theta = Math.atan(topBase/this.height);
+    	var mult = this.base;
 
-				this.vertices.push(ang1+(diffBaseTop*j), ang2+(diffBaseTop*j), j*height/this.stacks);
-				this.normals.push(ang1, ang2, theta);
-				this.textCoords.push(i*1/this.slices, j*height/this.stacks);
-			}
-		}
+    	for (let k = 0; k <= this.stacks; k++) {
+      		for (let i = 0; i <= this.slices; i++) {
 
-		for(let k = 0; k < (this.stacks*this.slices); k++){
+      			let ang1 = Math.cos(i * angle);
+      			let ang2 = Math.sin(i * angle);
 
-			if((k+1)%this.slices==0){
+      			this.vertices.push(ang1 * mult, ang2* mult, k*stackSize);
+        		this.normals.push(ang1, ang2, theta);
+        		this.texCoords.push(i/this.slices, k*stackSize);
+      		}
+      		mult = (k+1) * diff + this.base;
+    	}
 
-    			this.indices.push(k,k+1-this.slices, k+1);
-     	        this.indices.push(k,k+1, k+this.slices);
-     	        
-  			 }
+    	for (let k = 0; k <= this.stacks; k++) {
+      		for (let i = 0; i <= this.slices; i++) {
 
-  			 if((k+1)%this.slices!=0){
+        		if(k != 0 && i != 0) {
 
-  			 	 this.indices.push(k, k+1, k+1+this.slices);
-    			 this.indices.push(k, k+1+this.slices, k+this.slices);
-
-  			 }
-		}
-			
-		this.primitiveType = this.scene.gl.TRIANGLES;
-		this.initGLBuffers();
-	}
-}
+          			this.indices.push((this.slices+1)*k + i - 1, (this.slices+1)*(k-1) + i - 1, (this.slices+1)*(k-1) + i);
+					this.indices.push((this.slices+1)*k + i - 1, (this.slices+1)*(k-1) + i , (this.slices+1)*k + i);
+				
+					if(i == this.slices) {
+						this.indices.push((this.slices+1)*(k-1) + i, (this.slices+1)*(k-1), (this.slices+1)*k + i);
+						this.indices.push((this.slices+1)*k + i, (this.slices+1)*(k-1), (this.slices+1)*k);
+					}
+				}
+      		}
+    	}
+  	};
+};
